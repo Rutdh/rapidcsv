cd1a00c5842a724c6e5a94adf1ebc21066361c2b
jun.gan <1320957532@qq.com>
2024-07-01 23:00:38 +0800

    初步更改

--- examples/ex003.cpp
+++ examples/ex003.cpp
@@ -1,18 +1,15 @@
-#if 0
-TMP=$(mktemp -d)
-c++ -std=c++11 -I src -o ${TMP}/a.out ${0} && ${TMP}/a.out ${@:1} ; RV=${?}
-rm -rf ${TMP}
-exit ${RV}
-#endif
-
 #include <iostream>
 #include <vector>
 #include "rapidcsv.h"
 
 int main()
 {
-  rapidcsv::Document doc("examples/rowhdr.csv", rapidcsv::LabelParams(-1, 0));
+  rapidcsv::Document doc("./rowhdr.csv", rapidcsv::LabelParams(0, 0));
 
   std::vector<std::string> row = doc.GetRow<std::string>("2017-02-22");
   std::cout << "Read " << row.size() << " values." << std::endl;
+
+  std::cout << doc.GetRowCount() << std::endl;
+  // const std::string str("64.800003");
+  std::cout << doc.GetCell<double>("64.800003", 3)  << std::endl;
 }
